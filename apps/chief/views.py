e3a982418c18f3ab62eaece310d3f08512b1e0df
kell18 <albert.bikeev@gmail.com>
2016-11-30 18:40:01 +0300

    Add `group_required` decorator and urls forbidding

--- apps/chief/views.py
+++ apps/chief/views.py
@@ -5,7 +5,11 @@ from django.contrib.auth.decorators import login_required, permission_required
 from django.views.decorators.http import require_GET, require_POST
 from django.shortcuts import redirect
 from django.utils import timezone
+from metaord.utils.decorators import group_required
 
 
+login_url = '/login/'
+
+@group_required('chief', login_url=login_url)
 def index(request):
     return render(request, 'chief/index.html', {})
--- apps/worker/views.py
+++ apps/worker/views.py
@@ -6,33 +6,36 @@ from django.views.decorators.http import require_GET, require_POST
 from django.shortcuts import redirect
 from django.utils import timezone
 from metaord.utils.auth import Permissions, Groups
+from metaord.utils.decorators import group_required
 from metaord.models import Order
 from .forms import UserForm, OperatorForm
 
 
+login_url = '/login/'
+
+@group_required('worker', login_url=login_url)
 def index(request):
     return render(request, 'worker/index.html', {})
 
-login_url = '/worker/login/'
-
-@login_required(login_url=login_url)
+@group_required('worker', login_url=login_url)
 def orders_list(request):
     orders = Order.objects.filter(post_date__lte=timezone.now()).order_by('post_date')
     return render(request, 'worker/orders_list.html', {'orders': orders})
 
-@login_required(login_url=login_url)
-@permission_required('worker.change_order_status')
+@group_required('worker', login_url=login_url)
 def order(request, order_id):
     order = get_object_or_404(Order, id=order_id)
     return render(request, 'worker/order.html', {'order': order})
 
 @require_GET
+@group_required('worker', login_url=login_url)
 def register_operator_form(request):
     user_form = UserForm()
     oper_form = OperatorForm()
     return render(request, 'registration/register_operator.html', {'user_form': user_form, 'oper_form': oper_form})
 
 @require_POST
+@group_required('worker', login_url=login_url)
 def register_operator_submit(request):
     user_form = UserForm(request.POST)
     oper_form = OperatorForm(request.POST)
